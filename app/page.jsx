daa4e04bddc1e488d593d5a020a35de49280591b
ArangoMzl <sebastian.arango2411@gmail.com>
2024-01-26 18:56:54 -0500

    Create navbar and information taks

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,58 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import { FaCheckCircle } from "react-icons/fa";
+import { IoIosTime } from "react-icons/io";
+import { IoCopy } from "react-icons/io5";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      
-    </main>
+    <div className="flex gap-6">
+      <Card className="w-[500px] h-48 bg-[#ee78c7]/30 shadow-lg flex flex-col justify-between overflow-hidden">
+        <CardHeader className="mb-auto">
+          <CardTitle className="text-white font-bold text-2xl">
+            Tareas Restantes
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex justify-between items-center">
+          <span className="font-bold text-8xl text-[#4d0a2f] h-full">6</span>
+          <Image src="/3876654.png" alt="" width={220} height={130} className="flex relative -top-16 left-16 -rotate-12"/>
+        </CardContent>
+      </Card>
+      <Card className="flex w-screen h-48 bg-white/30 border border-[#ee78c7]/50 shadow-lg items-center ">
+        <CardContent className="flex w-full px-16 justify-between py-8">
+          <div className="">
+            <FaCheckCircle size={50} className="text-[#09E19E]" />
+            <span className="flex justify-center font-bold">Hecho</span>
+            <span className="flex font-bold text-3xl text-[#09E19E] justify-center">
+              12
+            </span>
+          </div>
+          <div className="">
+            <IoIosTime size={50} className="text-[#B978FF]" />
+            <span className="flex justify-center font-bold">En curso</span>
+            <span className="flex font-bold text-3xl text-[#B978FF] mx-2">
+              12
+            </span>
+          </div>
+          <div className="">
+            <IoCopy size={50} className="text-[#FE9D53]" />
+            <span className="flex justify-center font-bold">En fila</span>
+            <span className="flex font-bold text-3xl text-[#FE9D53] justify-center">
+              12
+            </span>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,100 @@
 import React from "react";
+import { GiBurningBook } from "react-icons/gi";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { FaPlus } from "react-icons/fa";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   return (
     <header className="py-2 backdrop-blur-md bg-white/30 rounded-full my-2">
-        <div>
-            <h1 className="text-3xl font-bold text-[#363636]/80 px-6 border-red-800 border-1">BRAINDSCAPE</h1>
+      <div className="flex flex-row justify-between px-6">
+        <div className="flex gap-2 text-[#E344AA] items-center">
+          <GiBurningBook size={32} className="cursor-pointer hover:size-10" />
+          <h1 className="text-3xl font-bold ">
+            <span className="text-white text-4xl">B</span>RAINDSCAPE
+          </h1>
         </div>
+        <div className="flex gap-3 items-center">
+          <IoMdNotificationsOutline size={24} className="hover:size-7 hover:text-[#E344AA] cursor-pointer"/>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>Mzl</AvatarFallback>
+              </Avatar> 
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  Profile
+                  <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Billing
+                  <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Settings
+                  <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Keyboard shortcuts
+                  <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuItem>Email</DropdownMenuItem>
+                      <DropdownMenuItem>Message</DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>More...</DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuItem>
+                  New Team
+                  <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>GitHub</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuItem disabled>API</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                Log out
+                <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Button variant="braindscape" className="gap-1.5">
+            <FaPlus />
+            Crear Nueva Tarea
+          </Button>
+        </div>
+      </div>
     </header>
   );
 }
